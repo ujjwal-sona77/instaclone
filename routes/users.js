d225c8ad1f40074dd9a30f39ede679d3540c8a83
Ujjwal Sonawat <146330690+ujjwal-sona77@users.noreply.github.com>
2024-10-25 14:19:13 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+mongoose.connect(process.env.MONGO_URL);
 
 const userSchema = mongoose.Schema({
   username: String,
